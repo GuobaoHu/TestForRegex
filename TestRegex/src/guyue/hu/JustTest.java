e81cefb8ad44e413e0702ce1615057f67e8485a3
guyuehu <398456458@qq.com>
2017-11-18 22:02:28 +0800

    更新JustTest.java和Mytest.java

--- TestRegex/src/guyue/hu/JustTest.java
+++ TestRegex/src/guyue/hu/JustTest.java
@@ -8,5 +8,6 @@ public class JustTest {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("good night");
+		System.out.println("I feel happy");
 	}
 }
